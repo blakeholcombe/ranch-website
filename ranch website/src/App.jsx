e86a085a4cff1e14ddb10d9f5ac29e843876de04
Blake Holcombe <bwholcombe@gmail.com>
2023-11-17 10:08:45 -0600

    clear the app and made a blank slate

--- ranch website/src/App.jsx
+++ ranch website/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,7 +7,8 @@ function App() {
   return (
     <>
       <div>
-        
+        Hello World! I need to do Much on this but I need to get back into coding in general so this is my best opportinuty and spot to start good luck to me! 
+    
       </div>
       <p >
       </p>
